f52d982ff4945663a37ecc41184cfa7be9477e09
AnirudhGithub1456 <97856022+AnirudhGithub1456@users.noreply.github.com>
2022-01-20 11:52:49 +1100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,47 @@
-var names = ["Anirudh", "Sam", "Jeff", "Sarah"]
-console.log(names)
-var marks = [70, 85, 90, 87, 40]
-console.log(marks)
-var sum = marks[0] + marks[1] + marks[2] + marks[3] + marks[4]
-var average = sum / marks.length
-console.log(average)
-console.log(sum)
-function setup() 
-{
+var sea,ship;
+var seaImg,shipImg;
+
+function preload(){
+  //uncomment the code to add animation to ship 
+
+  shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+  
+  seaImg = loadImage("sea.png");
+}
+
+function setup(){
   createCanvas(400,400);
+  background("blue");
+
+  // Moving background
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
+
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
 }
 
-function draw() 
-{
-background(51);
+function draw() {
+  background(0);
+  sea.velocityX = -3;
 
+  //uncomment code to reset the background
+  if(sea.x < 0){
+    //sea.x = 0;
+    //sea.x = sea.width;
+    sea.x = sea.width/8;
+    //sea.y = height;
+  }
+
+ 
+  drawSprites();
 }
-
