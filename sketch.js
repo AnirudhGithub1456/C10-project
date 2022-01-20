e51d6bc065f8e30e5779c01de7bd2138bcf5e310
AnirudhGithub1456 <97856022+AnirudhGithub1456@users.noreply.github.com>
2022-01-20 11:49:59 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var names = ["Anirudh", "Sam", "Jeff", "Sarah"]
+console.log(names)
+var marks = [70, 85, 90, 87, 40]
+console.log(marks)
+var sum = marks[0] + marks[1] + marks[2] + marks[3] + marks[4]
+var average = sum / marks.length
+console.log(average)
+console.log(sum)
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
